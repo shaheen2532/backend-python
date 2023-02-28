4e0c5f7cd0d208fb3782416e8e2adfb5aa54eb02
Muhammed Shaheen <shaheen2532@gmail.com>
2023-02-28 21:14:41 +0530

    migrations done

--- mproject/urls.py
+++ mproject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mproject import views
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path('view/', views.index),
-    path('view/page1', views.page1)
+    path('', views.index),
+    path('page1/', views.page1)
 ]
